b4d411c7964f1b28fde2645ae3fcf9d53aa5c633
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-15 19:21:38 -0400

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,7 @@
+export function Counter () {
+    return (
+        <div className="d-inline-flex bg-dark p-3">
+          <i className="fa-regular fa-clock text-white"></i><p className="text-white">Hola</p>
+        </div>
+    )
+}
